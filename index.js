12e994960e2ff41921253bef690cccd8fb8da426
fredysomy <fredysomy@gmail.com>
2024-11-07 15:33:59 +0530

    feat: added all

--- index.js
+++ index.js
@@ -1,37 +1,83 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const Todo = require("./models/todo");
 
 const app = express();
 
+app.use(express.json());
 
-app.use(express.json())
+const CON_STRING =
+  "mongodb+srv://fredy:fredy.csa2125@cluster0todo.f7lz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0Todo";
 
-const CON_STRING="mongodb+srv://fredy:fredy.csa2125@cluster0todo.f7lz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0Todo"
+mongoose
+  .connect(CON_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-mongoose.connect(CON_STRING,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    console.log("Connected to MongoDB")
-}).catch((err)=>{
-    console.log(err)
-})
+app.get("/hi", (req, res) => {
+  res.send("Hi Saintgits");
+});
 
+app.get("/todo", (req, res) => {
+  Todo.find()
+    .then((todos) => {
+      res.json({
+        todos: todos,
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
+app.post("/createtodo", (req, res) => {
+  const todo = new Todo({
+    todo: req.body.todo,
+    completed: false,
+  });
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
+  todo
+    .save()
+    .then((todo) => {
+      res.json({
+        success: true,
+        message: "Todo added successfully",
+        data: todo,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
+app.put("/updatetodo/:id", (req, res) => {
+  const id = req.params.id;
+  const { todo, status } = req.body;
 
-app.get("/hi",(req,res)=>{
-    res.send("Hi Saintgits")
-})
+  Todo.findByIdAndUpdate(id, {
+    todo: todo,
+    completed: status,
+  }).then((data) => {
+    res.json({
+      success: true,
+      message: "Todo updated successfully",
+      data: data,
+    });
+  });
+});
 
-
-app.listen(3000,()=>{   
-    console.log("Server is running on port 3000")
-})
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
